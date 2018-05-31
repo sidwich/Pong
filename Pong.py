0fe5c7ba44efd07cf2277e6d18535d1534cd7774
sidwich <39706045+sidwich@users.noreply.github.com>
2018-05-30 20:01:13 -0400

    Update Pong.py

--- Pong.py
+++ Pong.py
@@ -23,10 +23,10 @@ def bot():
     bl = 120
     pygame.draw.rect(screen, white, [by, bx, bw, bl])
 
-player()
-bot()
+
+
 xchange = 0
-pygame.display.flip()
+
 
 clock = pygame.time.Clock()
 done = False
@@ -37,20 +37,21 @@ while not done:
         elif event.type == pygame.KEYDOWN:
             if event.type == pygame.K_SPACE:
                 pause = True
-            if event.key == pygame.K_UP:
-                xchange = 3
             if event.key == pygame.K_DOWN:
-                xchange = -3
+                xchange = 5
+            if event.key == pygame.K_UP:
+                xchange = -5
         elif event.type == pygame.KEYUP:
             if event.key == pygame.K_UP:
                 xchange = 0
             if event.key == pygame.K_DOWN:
                 xchange = 0
+    if px <= 600 - 120 or px >= 0:
         px = px + xchange
-        pygame.display.flip()
-        clock.tick(60)
-        
-        
-        
-        
-        
+    else:
+        px = px
+    screen.fill(black)
+    player()
+    bot()
+    pygame.display.update()
+    clock.tick(60)
