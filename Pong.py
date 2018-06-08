79898064be17aa854dc8917c6347168e0dcac1e2
sidwich <39706045+sidwich@users.noreply.github.com>
2018-06-08 16:57:28 -0400

    Update Pong.py

--- Pong.py
+++ Pong.py
@@ -11,16 +11,17 @@ font_win = pygame.font.SysFont('Microsoft YaHei', 256)
 size = (1200, 600)
 screen = pygame.display.set_mode(size)
 titlename = pygame.image.load('PONGNAME.png')
-startimage = pygame.transform.scale(pygame.image.load('start.png'),(200,100))
-quitimage = pygame.transform.scale(pygame.image.load('quit.png'),(200, 100))
-pauseimage = pygame.transform.scale(pygame.image.load('resume.png'),(200, 100))
-restartimage = pygame.transform.scale(pygame.image.load('restart.png'),(200, 100))
+startimage = pygame.transform.scale(pygame.image.load('start.png'), (200, 100))
+quitimage = pygame.transform.scale(pygame.image.load('quit.png'), (200, 100))
+pauseimage = pygame.transform.scale(pygame.image.load('resume.png'), (200, 100))
+nextimage = pygame.transform.scale(pygame.image.load('next.png'), (200, 100))
+restartimage = pygame.transform.scale(pygame.image.load('restart.png'), (200, 100))
 intro = True
 paused = False
 done = False
 moving = False
-p1s = 0               #player1 score
-p2s = 0               #player2 score
+p1s = 0  # player1 score
+p2s = 0  # player2 score
 intro = True
 paused = False
 done = False
@@ -35,6 +36,8 @@ p1y = 45
 p2y = 1127
 xchange1 = 0
 xchange2 = 0
+p1r = 0
+p2r = 0
 
 clock = pygame.time.Clock()
 
@@ -45,21 +48,25 @@ def p1():
     p1l = 120
     pygame.draw.rect(screen, white, [p1y, p1x, p1w, p1l])
 
+
 def p2():
     p2y = 1127
     p2w = 25
     p2l = 120
     pygame.draw.rect(screen, white, [p2y, p2x, p2w, p2l])
 
-def ball(by,bx):
+
+def ball(by, bx):
     pygame.draw.ellipse(screen, white, [by, bx, 30, 30])
+
 
 def unpause():
     global paused
     paused = False
 
+
 def pause():
-    global  paused, moving
+    global paused, moving
     while paused == True:
         for pausemenu in pygame.event.get():
             if pausemenu.type == pygame.QUIT:
@@ -69,32 +76,66 @@ def pause():
                 if pausemenu.key == pygame.K_SPACE:
                     paused = False
 
-
         button(pauseimage, (1200 - 200) / 2, 200, 200, 100, black, black, unpause)
         button(quitimage, (1200 - 200) / 2, 400, 200, 100, black, black, quit)
-        button(restartimage, (1200-200)/2, 300, 200, 100, black, black, game_loop)
+        button(restartimage, (1200 - 200) / 2, 300, 200, 100, black, black, game_loop)
+        pygame.display.update()
+        clock.tick(60)
+
+def winround():
+    global p1s, p2s, winner, p1r, p2r
+    while winner == True:
+        for pausemenu in pygame.event.get():
+            if pausemenu.type == pygame.QUIT:
+                pygame.quit()
+                quit()
+        w2r = font.render(str(p2r), True, orange)
+        w1r = font.render(str(p2r), True, orange)
+        screen.fill(black)
+
+        if p1s >= 2:
+            win1 = font_win.render("P1WIN!", True, orange)
+            screen.blit(win1, (300, 50))
+            p1r += 1
+        if p2s >= 2:
+            win2 = font_win.render("P2WIN!", True, orange)
+            screen.blit(win2, (300, 50))
+            p2r += 1
+
+        screen.blit(w2r, (400, 500))
+        screen.blit(w1r, (800, 500))
+        p1()
+        p2()
+        scoreboard()
+        button(quitimage, (1200 - 200) / 2, 400, 200, 100, black, black, quit)
+        button(nextimage, (1200 - 200) / 2, 300, 200, 100, black, black, game_loop)
         pygame.display.update()
         clock.tick(60)
 
 def winner_winner_chicken_dinner():
     global p1s, p2s, winner
-    while winner == True:        
+    while winner == True:
         for pausemenu in pygame.event.get():
             if pausemenu.type == pygame.QUIT:
                 pygame.quit()
                 quit()
-        if p1s >= 2:
+
+        screen.fill(black)
+
+        if p1r >= 2:
             win1 = font_win.render("P1WIN!", True, orange)
             screen.blit(win1, (300, 50))
-        if p2s >= 2:
+        if p2r >= 2:
             win2 = font_win.render("P2WIN!", True, orange)
             screen.blit(win2, (300, 50))
         p1()
         p2()
+        scoreboard()
         button(quitimage, (1200 - 200) / 2, 400, 200, 100, black, black, quit)
         button(restartimage, (1200 - 200) / 2, 300, 200, 100, black, black, game_loop)
         pygame.display.update()
         clock.tick(60)
+
 
 def scoreboard():
     score1 = font.render(str(p1s), True, green)
@@ -103,7 +144,7 @@ def scoreboard():
     screen.blit(score2, (1000, 50))
 
 
-def button(img,x,y,w,h,ic,ac,action = None):
+def button(img, x, y, w, h, ic, ac, action=None):
     mouse = pygame.mouse.get_pos()
     click = pygame.mouse.get_pressed()
 
@@ -114,7 +155,7 @@ def button(img,x,y,w,h,ic,ac,action = None):
     else:
         pygame.draw.rect(screen, ic, (x, y, w, h))
 
-    screen.blit(img,(x,y))
+    screen.blit(img, (x, y))
 
 
 def intro():
@@ -123,8 +164,8 @@ def intro():
     while intro:
         screen.fill(black)
         screen.blit(titlename, (300, 50))
-        button(startimage,(1200-200)/2,300,200,100,black,black,game_loop)
-        button(quitimage, (1200-200)/2, 400, 200, 100, black,black,quit)
+        button(startimage, (1200 - 200) / 2, 300, 200, 100, black, black, game_loop)
+        button(quitimage, (1200 - 200) / 2, 400, 200, 100, black, black, quit)
         for startmenu in pygame.event.get():
             if startmenu.type == pygame.QUIT:
                 pygame.quit()
@@ -193,8 +234,6 @@ def game_loop():
         p1x = p1x + xchange1
         p2x = p2x + xchange2
 
-
-
         if moving == True:
             by += by_change
             bx += bx_change
@@ -202,16 +241,15 @@ def game_loop():
         if bx >= 600 - 30 or bx <= 0:
             bx_change = - bx_change
 
-        if by <= 70 and by >= 70-10:
-            if p1x <= bx and bx + 40 <= p1x+140:
+        if by <= 70 and by >= 70 - 10:
+            if p1x <= bx and bx + 40 <= p1x + 140:
                 by_change = -by_change
                 bx_change = random.randint(-10, 10)
 
-        if by >= 1127-30 and by <= 1127-30+10:
-            if p2x <= bx and bx + 40 <= p2x+140:
+        if by >= 1127 - 30 and by <= 1127 - 30 + 10:
+            if p2x <= bx and bx + 40 <= p2x + 140:
                 by_change = -by_change
                 bx_change = random.randint(-10, 10)
-
 
         if by <= 0:
             by = p2y - 32
@@ -231,28 +269,26 @@ def game_loop():
         if p1x < 0:
             p1x = 0
         if p2x > 600 - 120:
-            p2x = 600 -120
+            p2x = 600 - 120
         if p2x < 0:
             p2x = 0
 
         if p1s >= 2 or p2s >= 2:
             winner = True
-            winner_winner_chicken_dinner()
-        
+            winround()
+
         screen.fill(black)
         scoreboard()
         p1()
         p2()
-        ball(by,bx)
-        winner_winner_chicken_dinner()
+        ball(by, bx)
 
         pygame.display.update()
         clock.tick(60)
 
+
 intro()
 
-
-#球的反弹问题， 有一部分物理模型无效
-#p2发球问题， 出现glich
-#得分历史
-#音效（静音？）
+# 球的反弹问题， 有一部分物理模型无效
+# p2发球问题， 出现glich
+# 音效（静音？）
